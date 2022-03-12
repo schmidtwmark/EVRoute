28998ef771f37095652e93c76edcbf09794a5742
Mark Schmidt <mark.schmidt@hey.com>
2022-03-11 21:43:26 -0600

    Initial Commit

---
+++ EVRoute/EVRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVRouteApp.swift
+//  EVRoute
+//
+//  Created by Mark Schmidt on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct EVRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
